f141f7d1e6dc118d78cb89b74d89ca07a7551e8b
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-10-24 02:08:56 +0300

    C# Dersleri 7: Short Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -4,9 +4,14 @@
 //Integer 
 int number1 = 10;
 
+//short
+short number3 = -32768;
+
 //long
 long number2 = 21474836471231231;
 Console.WriteLine("Number1 is :{0}", number1);
 Console.WriteLine("Number2 is :{0}", number2);
+Console.WriteLine("Number3 is :{0}", number3);
+
 Console.ReadLine();
 
