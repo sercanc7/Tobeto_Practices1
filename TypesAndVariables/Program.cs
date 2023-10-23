fa1ab8dd7ec123e4aa87fd1fd39f5a45830b6e46
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-10-24 02:23:44 +0300

    C# Dersleri 11: Double Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -19,10 +19,13 @@ bool condition = true;
 //Char
 char character = 'A';
 
+//Double
+double number5 = 10.4;
 Console.WriteLine("Number1 is :{0}", number1);
 Console.WriteLine("Number2 is :{0}", number2);
 Console.WriteLine("Number3 is :{0}", number3);
 Console.WriteLine("Number4 is :{0}", number4);
+Console.WriteLine("Number5 is :{0}", number5);
 Console.WriteLine("Character is :{0}",(int)character);
 Console.ReadLine();
 
