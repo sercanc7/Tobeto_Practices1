51247fc2ea2a26f5309f5b4cdee6ea3804a847b3
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-10-24 02:04:34 +0300

    C# Dersleri 6: Long Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -3,6 +3,10 @@
 
 //Integer 
 int number1 = 10;
+
+//long
+long number2 = 21474836471231231;
 Console.WriteLine("Number1 is :{0}", number1);
+Console.WriteLine("Number2 is :{0}", number2);
 Console.ReadLine();
 
