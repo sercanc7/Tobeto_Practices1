63e1c291759e5d7ad2a218e13ec006fcdded7426
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-10-24 01:55:14 +0300

    C# Dersleri 5: Integer Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -1,3 +1,8 @@
 ﻿//Value Types
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+//Integer 
+int number1 = 10;
+Console.WriteLine("Number1 is :{0}", number1);
 Console.ReadLine();
+
