18e4c9676600c78276ffbc675cb2c09b1f992db5
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-10-24 02:46:53 +0300

    C# Dersleri 14: Var Keyword İle Çalışmak

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -24,11 +24,17 @@ double number5 = 10.4;
 
 //Decimal
 decimal number6 = 10.4m;
+
+// Var
+var number7 = 10;
+number7 = 'A';
+
 Console.WriteLine("Number1 is :{0}", number1);
 Console.WriteLine("Number2 is :{0}", number2);
 Console.WriteLine("Number3 is :{0}", number3);
 Console.WriteLine("Number4 is :{0}", number4);
 Console.WriteLine("Number5 is :{0}", number5);
+Console.WriteLine("Number7 is :{0}", number7);
 Console.WriteLine("Character is :{0}",(int)character);
 Console.WriteLine((int)Days.Friday);
 Console.ReadLine();
