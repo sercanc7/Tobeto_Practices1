c3ce43a5b24edc79ed497bc100a017ad64330b8f
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-10-24 03:54:55 +0300

    C# Dersleri 17: Else If Bloğuyla Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -1,4 +1,17 @@
-﻿var number = 10;
+﻿var number = 11;
 
-Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
+if (number==10)
+{
+    Console.WriteLine("Number is 10");
+}
+else if (number ==20)
+{
+    Console.WriteLine("Number is not 20");
+}
+
+else
+{
+    Console.WriteLine("Number is not 10 or 20");
+}
 Console.ReadLine();
+
