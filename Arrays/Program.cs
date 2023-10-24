733f4b99dcfdb51adf09146355200a3b5efaa805
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-10-24 13:59:39 +0300

    C# Dersleri 29: Çok Boyutlu Diziler

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -11,7 +11,28 @@ string[] students2 = { "Engin", "Derin", "Salih" };
 
 foreach (var student in students2)
 {
-    Console.WriteLine(student);
+    //Console.WriteLine(student);
 }
-Console.WriteLine();
+
+string[,] regions = new String[5, 3]
+{
+    {"İstanbul","İzmit","Balıkesir" },
+    {"Ankara","Konya","Kırıkkale" } ,
+    {"Antalya","Adana","Mersin" } ,
+    {"Rize","Trabzon","Samsun"} ,
+    {"İzmir","Muğla","Manisa"}
+
+};
+
+for (int i = 0; i <= regions.GetUpperBound(0); i++)
+{
+    for (int j = 0; j < regions.GetUpperBound(1); j++)
+    {
+        Console.WriteLine(regions[i, j]);
+    }
+
+    Console.WriteLine("******");
+}
+
+
 Console.ReadLine();
