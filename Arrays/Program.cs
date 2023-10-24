2c6bfad09fb5f9f0e4dbc31d1283740c88e79b5a
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-10-24 13:40:47 +0300

    C# Dersleri 28: Diziler

---
+++ Arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Uzun Metot
+string[] students = new String[3];
+students[0] = "Engin";
+students[1] = "Derin";
+students[2] = "Salih";
+
+
+// Kısa Metot
+string[] students2 = { "Engin", "Derin", "Salih" };
+
+foreach (var student in students2)
+{
+    Console.WriteLine(student);
+}
+Console.WriteLine();
+Console.ReadLine();
