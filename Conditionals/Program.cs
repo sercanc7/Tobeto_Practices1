c555921104b03448fba4ae13556ad2a829cde910
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-10-24 03:59:56 +0300

    C# Dersleri 18: Switch Bloğuyla Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -1,17 +1,33 @@
 ﻿var number = 11;
 
-if (number==10)
+//if (number==10)
+//{
+//    Console.WriteLine("Number is 10");
+//}
+//else if (number ==20)
+//{
+//    Console.WriteLine("Number is not 20");
+//}
+
+//else
+//{
+//    Console.WriteLine("Number is not 10 or 20");
+//}
+switch (number)
 {
-    Console.WriteLine("Number is 10");
-}
-else if (number ==20)
-{
-    Console.WriteLine("Number is not 20");
+    case 10:
+        Console.WriteLine("Number is 10");
+        break;
+
+    case 20:
+        Console.WriteLine("Number is 20");
+        break;
+
+    default:
+        Console.WriteLine("Number is not 10 or 20");
+        break;
 }
 
-else
-{
-    Console.WriteLine("Number is not 10 or 20");
-}
+
 Console.ReadLine();
 
