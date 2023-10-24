d76b8bc7764139766d5a7240b066f2cf216a8e64
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-10-24 12:59:02 +0300

    C# Dersleri 25: Out Keyword İle Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -7,9 +7,10 @@
         //Add();
         //var result = Add2(20);
 
+        // Out Keyword'da değer ataması yapıldan uygulanır,Ref'te zorunlu.
         int number1 = 20;
         int number2 = 100;
-        var result2 = Add3(ref number1, number2);
+        var result2 = Add3(out number1, number2);
         Console.WriteLine(result2);
         Console.WriteLine(number1);
         Console.ReadLine();
@@ -30,7 +31,7 @@
 
     }
 
-    static int Add3(ref int number1,int number2)
+    static int Add3(out  int number1,int number2)
     {
         number1 = 30;
         return number1 + number2;
