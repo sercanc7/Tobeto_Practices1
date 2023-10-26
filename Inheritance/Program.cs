4c45caf9140b89cd546e01ef79bc51a0ec0ab5a4
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-10-26 19:40:32 +0300

    C# Dersleri 46: Kalıtıma Giriş

---
+++ Inheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Headers;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer{FirstName = "Engin"} , 
+                new Student{FirstName = "Derin"} , 
+                new Person{FirstName="Sercan"}
+            };
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+        }
+    }
+
+
+    class Person
+    {
+        public int Id { get; set; } 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }    
+
+    }
+
+    class Customer:Person
+    {
+        public string City { get; set; }
+    }
+
+
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+}
