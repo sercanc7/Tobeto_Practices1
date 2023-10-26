231a8a44bc235228feee09680e5d9ca669bcb1f7
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-10-26 17:00:32 +0300

    Revert "C# Dersleri 41: İnterfacelere Temel Giriş"
    This reverts commit 87e3618827de6fbd1853513fad676beb36f5166b.

--- RecapDemo/Form1.cs
+++ RecapDemo/Form1.cs
@@ -10,9 +10,9 @@ namespace RecapDemo
         private void Form1_Load(object sender, EventArgs e)
         {
 
-            Button[,] buttons = new Button[8, 8];
-            int top = 0;
-            int left = 0;
+            Button[,] buttons = new Button[8,8];
+            int top=0;
+            int left=0;
             for (int i = 0; i < buttons.GetUpperBound(0); i++)
             {
                 for (int j = 0; j < buttons.GetUpperBound(1); j++)
@@ -21,21 +21,20 @@ namespace RecapDemo
                     buttons[i, j].Width = 50;
                     buttons[i, j].Height = 50;
                     buttons[i, j].Left = left;
-                    buttons[i, j].Top = top;
+                    buttons[i, j].Top = top;    
                     left += 50;
-
                     if ((i + j) % 2 == 0)
                     {
                         buttons[i, j].BackColor = Color.Black;
                     }
                     else
-                        buttons[i, j].BackColor = Color.White;
+                        buttons[i,j].BackColor = Color.White;
                     this.Controls.Add(buttons[i, j]);
-                }
+                }   
                 top += 50;
                 left = 0;
             }
-
+           
         }
     }
 }
