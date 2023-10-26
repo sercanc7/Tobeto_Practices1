fedc7d1e94b34341af36994102413033bd8a4f8e
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-10-26 15:11:09 +0300

    C# Dersleri 39: Encapsulation

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -4,8 +4,15 @@ namespace Classes
 {
     internal class Customer
     {
+        //Property
         public int Id { get; set; }
-        public string FirstName { get; set; }
+
+        private string _firstname ;
+        public string FirstName
+        {
+            get{ return "Mr." + _firstname; }
+            set{ _firstname = value; }
+        }
         public string LastName { get; set; }
         public string City {  get; set; }
     }
