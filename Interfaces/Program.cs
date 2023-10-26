87e3618827de6fbd1853513fad676beb36f5166b
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-10-26 16:58:10 +0300

    C# Dersleri 41: İnterfacelere Temel Giriş

---
+++ Interfaces/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    // Interface : Soyut Nesne
+    interface IPerson
+    {
+        int Id { get; set; }
+        int FirstName { get; set; }
+        int LastName { get; set; }
+
+    }
+
+    // class : Somut Nesne
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+
+        public string Address { get; set; }
+    }
+
+    class Student:IPerson
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        
+        public string Departmant {  get; set; }
+    }
+}
